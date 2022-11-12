9b519a1df41de85a336a7a912f1d5eb6097b67a5
Felix Berg <felixbpedersen@gmail.com>
2022-11-12 01:45:49 +0100

    fixed comments

--- include/algorithms/dijkstra.hpp
+++ include/algorithms/dijkstra.hpp
@@ -120,13 +120,13 @@ public:
 
         std::vector<bool> visited(graph.size(), 0);
 
-/* stack of finished priorityqueue elements*/
+        /* stack of finished priorityqueue elements*/
         std::vector<PQElm> finished;
 
-/* priority queue -> starts with the first node: from */
+        /* priority queue -> starts with the first node: from */
         std::deque<PQElm> pqueue { { from, via_none, EdgeLengthType() }};
 
-/* insert element into priority queue, sorted by current path length. */
+        /* insert element into priority queue, sorted by current path length. */
         const auto insert_sorted = [&pqueue](PQElm&& elm) {
             const auto p = std::find_if(pqueue.begin(), pqueue.end(),
                 [&elm](const PQElm& o)
@@ -144,7 +144,7 @@ public:
             visited[elm.node] = true;
 
             if (elm.node == to) {
-/* Found correct node, therefore shortest path. */
+                /* Found correct node, therefore shortest path. */
                 path_found = true;
                 break;
             }
@@ -161,7 +161,7 @@ public:
 
         if (!path_found) return std::nullopt;
 
-/* reconstruct path back to from */
+        /* reconstruct path back to from */
         const PQElm toelm = finished.back();
         PathType<DirectedGraph<D, N>> path { toelm.node };
         uint64_t viaidx = toelm.via_elmidx;
@@ -176,80 +176,3 @@ public:
     }
 };
 }; // namespace mazes
-
-
-//// HIGHER LEVEL IMPLEMENTATION: (1.5x slower)
-//template <typename EdgeLengthType = long, typename D, uint64_t N,
-//        CallableWithSignature<EdgeLengthType(uint64_t, uint64_t)> EdgeLength>
-//    /// Dijkstra's shortest path between from and to
-//    /// \tparam EdgeLengthType Return type of get_edge_length, the type of pathlengths
-//    /// \param get_edge_length A function that gets to adjacent nodes and computes the length of the edge between them
-//    /// \return Shortest path between nodes from and to, or std::nullopt, if no path is found
-//    static constexpr std::optional<PathType<DirectedGraph<D, N>>> search(
-//        const DirectedGraph<D, N>& graph,
-//        const uint64_t from, const uint64_t to,
-//        EdgeLength&& get_edge_length
-//        )
-//    {
-//        using PQElm = PQElement<EdgeLengthType>;
-//
-//        /* index indicating no connection */
-//constexpr uint64_t via_none = UINT64_MAX;
-//
-//std::vector<bool> visited(graph.size(), 0);
-//
-///* stack of finished priorityqueue elements*/
-//std::vector<PQElm> finished;
-//
-///* priority queue -> starts with the first node: from */
-//std::deque<PQElm> pqueue { { from, via_none, EdgeLengthType() }};
-//
-///* insert element into priority queue, sorted by current path length. */
-//const auto insert_sorted = [&pqueue](PQElm&& elm) {
-//    const auto p = std::find_if(pqueue.begin(), pqueue.end(),
-//        [&elm](const PQElm& o)
-//        { return elm.pathlen < o.pathlen; });
-//    pqueue.insert(p, elm);
-//};
-//
-//bool path_found = false;
-//while (!pqueue.empty()) {
-//const PQElm elm = pqueue.front();
-//pqueue.pop_front();
-//
-//finished.push_back(elm);
-//const uint64_t elmidx = finished.size() - 1;
-//visited[elm.node] = true;
-//
-//if (elm.node == to) {
-///* Found correct node, therefore shortest path. */
-//path_found = true;
-//break;
-//}
-//
-//const Edges auto & edges = graph.edges(elm.node);
-//for (uint64_t e : edges) {
-//if (visited[e]) continue;
-//
-//const EdgeLengthType el = get_edge_length(elm.node, e);
-//const EdgeLengthType pathlen = elm.pathlen + el;
-//insert_sorted({ e, elmidx, pathlen });
-//}
-//}
-//
-//if (!path_found) return std::nullopt;
-//
-///* reconstruct path back to from */
-//const PQElm toelm = finished.back();
-//PathType<DirectedGraph<D, N>> path { toelm.node };
-//uint64_t viaidx = toelm.via_elmidx;
-//while (viaidx != via_none) { /* until viaidx points to from */
-//path.push_back(finished.at(viaidx).node);
-//viaidx = finished.at(viaidx).via_elmidx;
-//}
-//
-//std::reverse(path.begin(), path.end());
-//
-//return path;
-//}
-////
