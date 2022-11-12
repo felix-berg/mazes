19d7d90121fa884c42c9afbbd31cc764ce7b6d46
Felix Berg <felixbpedersen@gmail.com>
2022-11-12 01:35:42 +0100

    dijsktra massive performance bump (replaced deque with vector)

--- include/algorithms/dijkstra.hpp
+++ include/algorithms/dijkstra.hpp
@@ -9,43 +9,134 @@
 #include <path_type.hpp>
 #include <callable.hpp>
 #include <deque>
+#include <concepts>
 
 namespace mazes {
-struct Dijkstra {
-public:
+class Dijkstra {
+    template <typename EdgeLengthType>
     struct PQElement {
-        uint64_t node;
-        uint64_t via_fi; /* index into "finished */
-        long pathlen;
+        uint64_t node; /* node of graph */
+        uint64_t via_elmidx; /* index into finished elements */
+        EdgeLengthType pathlen; /* combined pathlength up until node */
     };
 
-    template <typename D, uint64_t N, CallableWithSignature<long(uint64_t, uint64_t)> EdgeLength>
+public:
+    template <typename EdgeLengthType = long, typename D, uint64_t N,
+        CallableWithSignature<EdgeLengthType(uint64_t, uint64_t)> EdgeLength>
     /// Dijkstra's shortest path between from and to
+    /// \tparam EdgeLengthType Return type of get_edge_length, the type of pathlengths
     /// \param get_edge_length A function that gets to adjacent nodes and computes the length of the edge between them
     /// \return Shortest path between nodes from and to, or std::nullopt, if no path is found
-    static constexpr std::optional<PathType<DirectedGraph<D, N>>> search(
+    /// \remark 4097294 µs, 4.09729 seconds for 100000 iterations on 101x101 maze with >14000 solutions
+    ///         (more than double speed)
+    static constexpr std::optional<PathType<DirectedGraph<D,N>>> search(
         const DirectedGraph<D, N>& graph,
         const uint64_t from, const uint64_t to,
         EdgeLength&& get_edge_length
         )
     {
+        using PQElm = PQElement<EdgeLengthType>;
         constexpr uint64_t via_none = UINT64_MAX;
 
-        std::vector<bool> visited(graph.size(), 0);
-        std::vector<PQElement> finished;
-        std::deque<PQElement> pqueue { { from, via_none, 0 }};
+        /* Priority queue is split into two sections:
+         * |finished elements|priorityqueue|
+         *                    ^ beginidx
+         * finished nodes values are constant */
+        std::vector<PQElm> pqueue { { from, via_none, EdgeLengthType() } };
+        uint64_t beginidx = 0;
+        pqueue.reserve(graph.size() / 12 + 1);
 
-        const auto insert_sorted = [&pqueue](PQElement&& elm) {
-            const auto p = std::find_if(pqueue.begin(), pqueue.end(),
-                         [&elm](const PQElement& o)
-                         { return elm.pathlen < o.pathlen; });
+        std::vector<bool> visited(graph.size());
+
+        /* insert element into sorted priority queue (by pathlen)*/
+        const auto insert_sorted = [&pqueue, &beginidx](PQElm&& elm) {
+            const auto p = std::find_if(
+                pqueue.begin() + beginidx, pqueue.end(),
+                [&elm](const PQElm& other) {
+                    return elm.pathlen < other.pathlen;
+            });
             pqueue.insert(p, elm);
         };
 
-        bool success = false;
-        while (!pqueue.empty()) { /* "while not empty" */
-            /* "pop front" */
-            const PQElement elm = pqueue.front();
+        bool path_found = false;
+        while (beginidx < pqueue.size()) { /* while queue is not empty*/
+            /* "pop front" (and push to finished) */
+            const PQElm& element = pqueue.at(beginidx);
+            const uint64_t elmidx = beginidx;
+            beginidx++;
+
+            if (element.node == to) {
+                path_found = true;
+                break;
+            }
+
+            visited[element.node] = true;
+
+            const Edges auto& edges = graph.edges(element.node);
+            for (const uint64_t e : edges) {
+                if (visited[e]) continue;
+                const EdgeLengthType elen = get_edge_length(element.node, e);
+                const EdgeLengthType pathlen = element.pathlen + elen;
+                insert_sorted(PQElm(e, elmidx, pathlen));
+            }
+        }
+
+        if (!path_found) return std::nullopt;
+
+        const uint64_t finished_end = beginidx;
+        const PQElm& to_elm = pqueue.at(finished_end - 1);
+
+        PathType<DirectedGraph<D,N>> path = { to_elm.node };
+        path.reserve(graph.size() / 24 + 1);
+
+        /* reconstruct path from last node, moving backward through via_elmidx */
+        uint64_t viaidx = to_elm.via_elmidx;
+        while (viaidx != via_none) {
+            const PQElm& elm = pqueue.at(viaidx);
+            path.push_back(elm.node);
+            viaidx = elm.via_elmidx;
+        }
+
+        return path;
+    }
+
+    template <typename EdgeLengthType = long, typename D, uint64_t N,
+        CallableWithSignature<EdgeLengthType(uint64_t, uint64_t)> EdgeLength>
+    /// Dijkstra's shortest path between from and to
+    /// \tparam EdgeLengthType Return type of get_edge_length, the type of pathlengths
+    /// \param get_edge_length A function that gets to adjacent nodes and computes the length of the edge between them
+    /// \return Shortest path between nodes from and to, or std::nullopt, if no path is found
+    /// \remark 10977375 µs, 10.9774 seconds for 100000 iterations on 101x101 maze with >14000 solutions
+    static constexpr std::optional<PathType<DirectedGraph<D, N>>> search_deque(
+        const DirectedGraph<D, N>& graph,
+        const uint64_t from, const uint64_t to,
+        EdgeLength&& get_edge_length
+    )
+    {
+        using PQElm = PQElement<EdgeLengthType>;
+
+        /* index indicating no connection */
+        constexpr uint64_t via_none = UINT64_MAX;
+
+        std::vector<bool> visited(graph.size(), 0);
+
+/* stack of finished priorityqueue elements*/
+        std::vector<PQElm> finished;
+
+/* priority queue -> starts with the first node: from */
+        std::deque<PQElm> pqueue { { from, via_none, EdgeLengthType() }};
+
+/* insert element into priority queue, sorted by current path length. */
+        const auto insert_sorted = [&pqueue](PQElm&& elm) {
+            const auto p = std::find_if(pqueue.begin(), pqueue.end(),
+                [&elm](const PQElm& o)
+                { return elm.pathlen < o.pathlen; });
+            pqueue.insert(p, elm);
+        };
+
+        bool path_found = false;
+        while (!pqueue.empty()) {
+            const PQElm elm = pqueue.front();
             pqueue.pop_front();
 
             finished.push_back(elm);
@@ -53,8 +144,8 @@ public:
             visited[elm.node] = true;
 
             if (elm.node == to) {
-                /* Found correct node, therefore shortest path. */
-                success = true;
+/* Found correct node, therefore shortest path. */
+                path_found = true;
                 break;
             }
 
@@ -62,21 +153,21 @@ public:
             for (uint64_t e : edges) {
                 if (visited[e]) continue;
 
-                const long el = get_edge_length(elm.node, e);
-                const long pathlen = elm.pathlen + el;
+                const EdgeLengthType el = get_edge_length(elm.node, e);
+                const EdgeLengthType pathlen = elm.pathlen + el;
                 insert_sorted({ e, elmidx, pathlen });
             }
         }
 
-        if (!success) return std::nullopt;
+        if (!path_found) return std::nullopt;
 
-        /* reconstruct path from */
-        const PQElement toelm = finished.back();
+/* reconstruct path back to from */
+        const PQElm toelm = finished.back();
         PathType<DirectedGraph<D, N>> path { toelm.node };
-        uint64_t viaidx = toelm.via_fi;
-        while (viaidx != via_none) {
+        uint64_t viaidx = toelm.via_elmidx;
+        while (viaidx != via_none) { /* until viaidx points to from */
             path.push_back(finished.at(viaidx).node);
-            viaidx = finished.at(viaidx).via_fi;
+            viaidx = finished.at(viaidx).via_elmidx;
         }
 
         std::reverse(path.begin(), path.end());
@@ -85,3 +176,80 @@ public:
     }
 };
 }; // namespace mazes
+
+
+//// HIGHER LEVEL IMPLEMENTATION: (1.5x slower)
+//template <typename EdgeLengthType = long, typename D, uint64_t N,
+//        CallableWithSignature<EdgeLengthType(uint64_t, uint64_t)> EdgeLength>
+//    /// Dijkstra's shortest path between from and to
+//    /// \tparam EdgeLengthType Return type of get_edge_length, the type of pathlengths
+//    /// \param get_edge_length A function that gets to adjacent nodes and computes the length of the edge between them
+//    /// \return Shortest path between nodes from and to, or std::nullopt, if no path is found
+//    static constexpr std::optional<PathType<DirectedGraph<D, N>>> search(
+//        const DirectedGraph<D, N>& graph,
+//        const uint64_t from, const uint64_t to,
+//        EdgeLength&& get_edge_length
+//        )
+//    {
+//        using PQElm = PQElement<EdgeLengthType>;
+//
+//        /* index indicating no connection */
+//constexpr uint64_t via_none = UINT64_MAX;
+//
+//std::vector<bool> visited(graph.size(), 0);
+//
+///* stack of finished priorityqueue elements*/
+//std::vector<PQElm> finished;
+//
+///* priority queue -> starts with the first node: from */
+//std::deque<PQElm> pqueue { { from, via_none, EdgeLengthType() }};
+//
+///* insert element into priority queue, sorted by current path length. */
+//const auto insert_sorted = [&pqueue](PQElm&& elm) {
+//    const auto p = std::find_if(pqueue.begin(), pqueue.end(),
+//        [&elm](const PQElm& o)
+//        { return elm.pathlen < o.pathlen; });
+//    pqueue.insert(p, elm);
+//};
+//
+//bool path_found = false;
+//while (!pqueue.empty()) {
+//const PQElm elm = pqueue.front();
+//pqueue.pop_front();
+//
+//finished.push_back(elm);
+//const uint64_t elmidx = finished.size() - 1;
+//visited[elm.node] = true;
+//
+//if (elm.node == to) {
+///* Found correct node, therefore shortest path. */
+//path_found = true;
+//break;
+//}
+//
+//const Edges auto & edges = graph.edges(elm.node);
+//for (uint64_t e : edges) {
+//if (visited[e]) continue;
+//
+//const EdgeLengthType el = get_edge_length(elm.node, e);
+//const EdgeLengthType pathlen = elm.pathlen + el;
+//insert_sorted({ e, elmidx, pathlen });
+//}
+//}
+//
+//if (!path_found) return std::nullopt;
+//
+///* reconstruct path back to from */
+//const PQElm toelm = finished.back();
+//PathType<DirectedGraph<D, N>> path { toelm.node };
+//uint64_t viaidx = toelm.via_elmidx;
+//while (viaidx != via_none) { /* until viaidx points to from */
+//path.push_back(finished.at(viaidx).node);
+//viaidx = finished.at(viaidx).via_elmidx;
+//}
+//
+//std::reverse(path.begin(), path.end());
+//
+//return path;
+//}
+////
--- main.cpp
+++ main.cpp
@@ -15,18 +15,33 @@ int main() {
     });
 
     const MazeGraph graph = graph_from_maze(maze);
-    const auto path =
-        Dijkstra::search(graph, 0, graph.size() - 1,
-            [&graph](const uint64_t f, const uint64_t t) -> long {
-                const Point p = graph.node(f),
-                            o = graph.node(t);
-                if (p.x == o.x)
-                    return std::abs(int(p.y - o.y));
-                else
-                    return std::abs(int(p.x - o.x));
-            }
-        ).value();
 
+    clock_t start_t = clock(), end_t;
+    uint64_t iterations = 100000;
+
+    PathType<MazeGraph> path;
+    for (uint64_t i = 0; i < iterations; i++) {
+        path =
+            Dijkstra::search<long>(graph, 0, graph.size() - 1,
+                [&graph](const uint64_t f, const uint64_t t) -> long {
+                    const Point p = graph.node(f),
+                                o = graph.node(t);
+                    if (p.x == o.x)
+                        return std::abs(int(p.y - o.y));
+                    else
+                        return std::abs(int(p.x - o.x));
+                }
+            ).value();
+        assert(path.size() != 0);
+    }
+
+    end_t = clock();
+    std::cout << "Took " << (end_t - start_t) << " µs, "
+              << float(end_t - start_t) / float(CLOCKS_PER_SEC) << " seconds.\n";
+
+
+    const auto paths = find_all_paths(graph, 0, graph.size() - 1);
+    std::cout << "Solutions: " << paths.size() << '\n';
 
     fbg::LoopWin win ("Maze solver", 800, 800);
 
